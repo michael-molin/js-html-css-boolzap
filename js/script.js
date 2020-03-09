f80ab4ce20abb82746e520980a0e4dcfda99fee4
Michael <m.molin.1987@gmail.com>
2020-03-09 15:38:28 +0100

    test cancellazione messaggi

--- js/script.js
+++ js/script.js
@@ -9,10 +9,42 @@ $(document).ready(function(){
     if(orarioOre < 10) {
         orarioOre = '0'+orarioMinuti;
     }
-    
+
     var orarioAttuale = (orarioOre + ":" + orarioMinuti);
 
+        //Gestione ICONA mouseenter mouseleave
+        $('.chat').on('mouseenter', '.msg' , function() {
+            $(this).find('.msg-overlay').show();
+            $(this).find('.menu-opzioni').hide();
+
+        });
+
+        $('.chat').on('mouseleave', '.msg' , function() {
+            console.log('mouseleave su msg');
+            $(this).find('.msg-overlay').hide()
+        });
+
+
+        // GESTIONE apertura opzioni chat
+        $('.chat').on('click' , '.opzioni-chat i' , function() {
+            console.log('click opzioni chat');
+            $(this).next('.menu-opzioni').toggle();
+        })
+
+        $('.chat').on('click' , '.cancella-chat' , function() {
+            console.log('click cancella chat');
+            var checkposition = $(this).parentsUntil('.msg');
+            console.log(checkposition);
+            $('this').parentsUntil('.msg').hide();
+        })
+
+
+
+
+
         $('.utente').click(function(){
+            $('.utente').removeClass('utenteclick');
+            $(this).addClass('utenteclick');
             var dataUtenteTemp = $(this).data('nomeUtente');
             var nomeChat = $(this).find('h4').text();
             var orarioChat = $(this).find('h5').text();
@@ -21,6 +53,7 @@ $(document).ready(function(){
                 if(dataUtenteTemp == ($(this).data('nomeUtente'))) {
                     $('.chat').removeClass('chat-active');
                     $(this).addClass('chat-active');
+                    $('.testo').addClass('testo-active');
                     $('.destinatario-chat-nome h4').text(nomeChat);
                     $('.destinatario-chat-nome p').text('Ultimo accesso alle: ' + orarioChat);
                     $('.destinatario-chat-img').html(imgUtente);
@@ -62,14 +95,16 @@ $(document).ready(function(){
         function inviaMessaggio() {
             var inputSalvato = $('#input-msg').val();
             $('#input-msg').val('');
-            var nuovoMessaggio = $('.template .msg-sent').clone();
+            var nuovoMessaggio = $('.template .msg').clone();
+            nuovoMessaggio.addClass('msg-sent');
             creaMsg(nuovoMessaggio, inputSalvato, orarioAttuale);
             scroll();
         }
 
         function rispostaMessaggio() {
             setTimeout(function () {
-                var nuovoMessaggio = $('.template .msg-receive').clone();
+                var nuovoMessaggio = $('.template .msg').clone();
+                nuovoMessaggio.addClass('msg-receive');
                 creaMsg(nuovoMessaggio ,'ok', orarioAttuale);
                 $('.destinatario-chat-nome p').text('Ultimo accesso alle: ' + orarioAttuale);
                 scroll();
@@ -78,7 +113,6 @@ $(document).ready(function(){
 
         function scroll() {
             var pixelScroll =$('.chat.chat-active').height();
-            console.log(pixelScroll);
             $('.chat.chat-active').scrollTop(pixelScroll);
         }
 
